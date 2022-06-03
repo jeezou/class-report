057de7b381cf3e1f22241ffb4f68487ed70ef463
pugocoder <57970164+pugocoder@users.noreply.github.com>
2022-06-03 17:57:23 +0300

    init: start test project

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 5000;
+
+const db = require("./queries");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// app.get("/", (request, response) => {
+//   response.json({ info: "Node.js, Express, and Postgres API" });
+// });
+
+app.get("/", db.getClasses);
+app.get("/lessons", db.createLessons);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
